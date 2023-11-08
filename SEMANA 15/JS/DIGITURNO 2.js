90336a590a9c5aa61c9ff84804b2afe28afe5e30
HAROLDGUTIERREZ0210 <141885457+HAROLDGUTIERREZ0210@users.noreply.github.com>
2023-11-07 21:40:24 -0500

    Add files via upload

---
+++ SEMANA 15/JS/DIGITURNO 2.js
@@ -0,0 +1,12 @@
+var turno=['F01','P01','Q01','M01','F02','P02'];
+var modulo=['01','02','03','04','05','06'];
+function tick(){
+    let con=1;
+    let ce=0;
+    while (con<=6){
+        document.getElementById("t"+con).value=turno[e];
+        document.getElementById("m"+con).value=modulo[e];
+        ++ce;
+        ++con;
+    }
+}
